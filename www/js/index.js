9c0b7479abce6e367a49c612fedc79fe9cc3de5c
ddgiacomo <dd_giacomo@hotmail.com>
2016-12-05 19:23:07 -0400

    Add files via upload

--- www/js/index.js
+++ www/js/index.js
@@ -222,8 +222,8 @@ if (sh0 == "1"){
 	var texto=umail;
 	var texto2=uname;
 	var texto3=uphon;
-//	var texto4="Esta es una prueba de TSC, version Android";
-	var texto4 = "X: " + x + " XH: " + xh + " IW: " + iw + " IH: " + ih;
+	var texto4="True Shot CAM, version Android";
+//	var texto4 = "X: " + x + " XH: " + xh + " IW: " + iw + " IH: " + ih;
 	var texto5="Coords: -67.02737, 10.60886";
 	var texto6="Direccion 1";
 	var texto7="Direccion 2";
